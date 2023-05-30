787ef39a8c1f3abb11aad427c321f3da2d36dee3
Zuruuh <ziadi.mail.pro@gmail.com>
2023-05-30 18:00:34 +0200

    oeoeoe

--- .config/nvim/lua/zuruh/packer.lua
+++ .config/nvim/lua/zuruh/packer.lua
@@ -48,7 +48,7 @@ return require('packer').startup(function(use)
             require('gitsigns').setup()
         end
     }
-    use('andweeb/presence.nvim')
+    -- use('andweeb/presence.nvim')
     use('nvim-lualine/lualine.nvim')
     use('romgrk/barbar.nvim')
     use('lukas-reineke/indent-blankline.nvim')
