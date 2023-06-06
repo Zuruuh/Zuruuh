e1b7c34ef284bc27a4c4e7625d71ea5864193ee6
Zuruuh <ziadi.mail.pro@gmail.com>
2023-06-06 17:08:59 +0200

    yeee switch dev ez

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,3 +1,12 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>");
 vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>");
+vim.keymap.set("n", "<leader>bd", function () os.execute([[
+    command git rev-parse --git-dir &> /dev/null || return
+    for branch in dev develop development; do
+        if command git show-ref -q --verify refs/heads/$branch; then
+            git switch $branch
+            break
+        fi;
+    done;
+]]) end)
