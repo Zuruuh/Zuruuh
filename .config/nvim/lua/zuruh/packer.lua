45c6eb45f5a71045065362da79367d342a9a4be3
Zuruuh <ziadi.mail.pro@gmail.com>
2023-06-06 09:38:35 +0200

    do some more stuff lol

--- .config/nvim/lua/zuruh/packer.lua
+++ .config/nvim/lua/zuruh/packer.lua
@@ -3,8 +3,15 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use('wbthomason/packer.nvim')
     use({
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = { {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-live-grep-args.nvim'
+        } },
+        config = function ()
+            require('telescope').load_extension('live_grep_args')
+        end
     })
     use({
         'navarasu/onedark.nvim',
