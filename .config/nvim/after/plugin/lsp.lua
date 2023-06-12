5d94948e3b327d17f1ffd6018935aa0d4b669fd1
Zuruuh <ziadi.mail.pro@gmail.com>
2023-06-12 14:37:30 +0200

    Update autoformat, fix diagnostic for non-eslint messages add little message when closing vim c:

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -62,6 +62,13 @@ lsp.set_preferences({
     }
 })
 
+lsp.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 2000
+    }
+})
+
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
@@ -75,16 +82,16 @@ lsp.on_attach(function(client, bufnr)
                         diagnostic.user_data.lsp.code
                     )
                 end
+
+                return diagnostic.message
             end
         }
     })
-    vim.keymap.set("n", "<leader>f", "<cmd>EslintFixAll<CR>")
-    vim.keymap.set("n", "<leader>hf", "<cmd>EslintFixAll<CR>")
+    vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>vad", function() vim.diagnostic.show() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
