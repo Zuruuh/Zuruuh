86e9eb40283c444dce271022f30b01f31b5e5ab2
Zuruuh <ziadi.mail.pro@gmail.com>
2023-06-28 17:56:02 +0200

    Configure native fzf for faster & flexible search

--- .config/nvim/after/plugin/finder.lua
+++ .config/nvim/after/plugin/finder.lua
@@ -1,6 +1,20 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
+telescope.setup({
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+        }
+    }
+})
+
+telescope.load_extension('live_grep_args')
+telescope.load_extension('fzf')
+
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, {})
--- .config/nvim/lua/zuruh/packer.lua
+++ .config/nvim/lua/zuruh/packer.lua
@@ -7,11 +7,9 @@ return require('packer').startup(function(use)
         tag = '0.1.0',
         requires = { {
             'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-live-grep-args.nvim'
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
         } },
-        config = function ()
-            require('telescope').load_extension('live_grep_args')
-        end
     })
     use({
         'navarasu/onedark.nvim',
