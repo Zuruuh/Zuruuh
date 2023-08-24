59a1c391f0c85de6760b975528964fc83acc3955
Zuruuh <ziadi.mail.pro@gmail.com>
2023-08-24 07:27:04 +0200

    Update explorer config

--- .config/nvim/lua/zuruh/remaps.lua
+++ .config/nvim/lua/zuruh/remaps.lua
@@ -1,4 +1,2 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
-vim.keymap.set("n", "<leader>rl", "<CMD>so ~/.config/nvim/init.lua<CR>")
-vim.keymap.set("n", "<leader>q", "<CMD>q<CR>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
