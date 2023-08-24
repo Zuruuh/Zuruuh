787cb6add50b611b22be16c78f7c775a46791e2d
Zuruuh <ziadi.mail.pro@gmail.com>
2023-08-24 08:15:49 +0200

    Add inline folding plugin

---
+++ .config/nvim/after/plugin/fold.lua
@@ -0,0 +1,15 @@
+local ufo = require("ufo")
+
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.keymap.set("n", "zR", ufo.openAllFolds)
+vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+ufo.setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
+})
