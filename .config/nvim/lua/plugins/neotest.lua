d84c34ee32747fb93927202022279922ea6676f2
Zuruuh <ziadi.mail.pro@gmail.com>
2023-08-25 20:53:14 +0200

    yes

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,28 @@
+return {
+	"nvim-neotest/neotest",
+	lazy = true,
+	dependencies = {
+		"olimorris/neotest-phpunit",
+		"marilari88/neotest-vitest",
+		"rouge8/neotest-rust",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-vitest"),
+				require("neotest-rust"),
+				require("neotest-phpunit")({
+					phpunit_cmd = function()
+						local binary = io.open("vendor/bin/phpunit", "r")
+						if binary ~= nil then
+							io.close(binary)
+							return "vendor/bin/phpunit"
+						else
+							return "bin/phpunit"
+						end
+					end,
+				}),
+			},
+		})
+	end,
+}
