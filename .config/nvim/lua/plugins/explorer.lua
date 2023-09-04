a1710dcbdac5305e49a15e7dfcb576f4831919d6
Zuruuh <ziadi.mail.pro@gmail.com>
2023-09-04 10:50:10 +0200

    Setup neotree

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -1,4 +1,4 @@
-vim.g.loaded_netrw = 1
+--[[vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 return {
@@ -17,5 +17,28 @@ return {
 		view = {
 			side = "right",
 		},
+	},
+}
+]]
+--
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>pv",
+			vim.cmd.Neotree,
+		},
+	},
+	opts = {
+		window = {
+			position = "right",
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
 	},
 }
--- .config/nvim/lua/zuruh/remaps.lua
+++ .config/nvim/lua/zuruh/remaps.lua
@@ -1,3 +1,1 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
-vim.keymap.set("n", "<c-n>", vim.cmd.NvimTreeFindFileToggle)
