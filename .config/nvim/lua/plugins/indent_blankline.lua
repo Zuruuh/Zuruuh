8d1d0a321679647e819546be90265817d4419d22
Zuruuh <ziadi.mail.pro@gmail.com>
2023-10-02 10:31:31 +0200

    Remove old ibl config

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,8 +1,8 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	opts = {
-		show_current_context = true,
-		show_current_context_start = true,
-	},
+	-- opts = {
+	-- show_current_context = true,
+	-- show_current_context_start = true,
+	-- },
 }
