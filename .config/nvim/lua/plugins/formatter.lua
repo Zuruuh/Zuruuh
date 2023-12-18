248c24289ff8af23e945fdd8169f4b11d9f8f68a
Zuruuh <ziadi.mail.pro@gmail.com>
2023-12-18 14:34:15 +0100

    Disable php formatter

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -33,10 +33,10 @@ return {
 				toml = require("formatter.filetypes.toml").taplo,
 				-- yaml = prettier(),
 				rust = require("formatter.filetypes.rust").rustfmt,
-				php = {
-					-- require("formatter.filetypes.php").phpcbf,
-					require("formatter.filetypes.php").php_cs_fixer,
-				},
+				-- php = {
+				-- require("formatter.filetypes.php").phpcbf,
+				-- require("formatter.filetypes.php").php_cs_fixer,
+				-- },
 
 				["*"] = {
 					require("formatter.filetypes.any").remove_trailing_whitespace,
