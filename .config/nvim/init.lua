4b606724de94939b27fb878a48ce915196aa7299
Zuruuh <zuruh.dev@proton.me>
2024-04-02 03:32:09 +0200

    add CTRL+e autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -641,6 +641,7 @@ require('lazy').setup({
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
           ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-e>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
