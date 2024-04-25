fcaf2286b10fbea2dd285b11873ac36818cdd935
Zuruuh <zuruh.dev@proton.me>
2024-04-25 15:22:11 +0000

    move XDG_* vars to `wsl.nix` instead of `packages.nix`

--- root/etc/nixos/packages.nix
+++ root/etc/nixos/packages.nix
@@ -109,11 +109,4 @@ in
     zstd
     zulu17
   ];
-
-  environment.sessionVariables = {
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
-  };
 }
--- root/etc/nixos/wsl.nix
+++ root/etc/nixos/wsl.nix
@@ -44,6 +44,12 @@ in
     };
   };
 
+  environment.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
 
   security.sudo.wheelNeedsPassword = true;
 
