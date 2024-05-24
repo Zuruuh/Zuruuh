d07a073d6045fb90f58846d244fc5ad9321384c9
Zuruuh <zuruh.dev@proton.me>
2024-05-24 01:44:22 +0000

    ignore javascript snippets

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -612,7 +612,7 @@ require('lazy').setup({
         'rafamadriz/friendly-snippets',
         config = function()
           require('luasnip.loaders.from_vscode').lazy_load({
-            exclude = { 'php', 'rust', 'global' },
+            exclude = { 'php', 'rust', 'javascript', 'global' },
           })
         end,
       },
