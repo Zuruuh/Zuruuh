c43fc911b63d603bafcfe568346c6ee6ae48b210
Zuruuh <zuruh.dev@proton.me>
2024-07-16 17:23:31 +0000

    Update neovim plugins and migrate which key config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -220,15 +220,16 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require('which-key')
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').register({
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      wk.add({
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
       })
     end,
   },
@@ -950,6 +951,9 @@ require('lazy').setup({
     },
   },
 }, {
+  rocks = {
+    enabled = false,
+  },
   ui = {
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
