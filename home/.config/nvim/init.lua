a28151cd9ade1f4310b80cb3cae7d57f14c25521
Zuruuh <zuruh.dev@proton.me>
2024-12-15 18:33:11 +0000

    Install lspkinds for lsp completion symbols Re-install markview since wezterm is goated

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -577,6 +577,9 @@ require('lazy').setup({
       --     })
       --   end,
       -- },
+
+      -- LSP Symbols in completions menu
+      'onsails/lspkind.nvim',
     },
     config = function()
       -- See `:help cmp`
@@ -638,6 +641,27 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'crates' },
+        },
+        ---@diagnostic disable-next-line: missing-fields
+        formatting = {
+          format = require('lspkind').cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = {
+              -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+              -- can also be a function to dynamically calculate max width such as
+              -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+              -- menu = 50, -- leading text (labelDetails)
+              -- abbr = 50, - actual suggestion item
+            },
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(_, vim_item)
+              -- ...
+              return vim_item
+            end,
+          }),
         },
       })
     end,
@@ -929,6 +953,7 @@ require('lazy').setup({
       {
         '<leader>go',
         function()
+          ---@diagnostic disable-next-line: undefined-global
           Snacks.gitbrowse.open()
         end,
         silent = true,
@@ -938,16 +963,16 @@ require('lazy').setup({
   },
   -- Disabled until alacritty stops breaking on links
   -- or switching to another terminal
-  -- {
-  --   'OXY2DEV/markview.nvim',
-  --   lazy = false, -- Recommended
-  --   -- ft = "markdown" -- If you decide to lazy-load anyway
-  --
-  --   dependencies = {
-  --     'nvim-treesitter/nvim-treesitter',
-  --     'nvim-tree/nvim-web-devicons',
-  --   },
-  -- },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 }, {
   rocks = {
     enabled = false,
