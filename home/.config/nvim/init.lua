7913fa339b072e5028c8461837a2f5c184c85854
Zuruuh <zuruh.dev@proton.me>
2025-01-06 10:53:32 +0100

    Disable broken biome_or_prettier formatter config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -126,12 +126,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
@@ -540,14 +540,6 @@ require('lazy').setup({
         }
       end,
 
-      biome_or_prettier = function()
-        return {
-          command = require('conform.util').find_executable({
-            'node_modules/.bin/biome',
-          }, 'prettier'),
-        }
-      end,
-
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'biome' },
@@ -559,7 +551,7 @@ require('lazy').setup({
         astro = { 'biome' },
         html = { 'prettier' },
         css = { 'biome' },
-        json = { 'biome_or_prettier' },
+        json = { 'biome', lsp_format = 'fallback' },
         markdown = { 'deno_fmt' },
         sh = { 'shfmt' },
         toml = { 'taplo' },
