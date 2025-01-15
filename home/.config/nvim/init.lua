a81bc2e738008a0cc294939f587ce8d507db1f11
Zuruuh <zuruh.dev@proton.me>
2025-01-15 15:35:55 +0000

    Remove git branch from statusline

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -375,8 +375,8 @@ require('lazy').setup({
         php_cs_fixer = function()
           return {
             command = require('conform.util').find_executable({
-              'vendor/bin/php-cs-fixer',
               'bin/php-cs-fixer',
+              'vendor/bin/php-cs-fixer',
             }, 'php-cs-fixer'),
           }
         end,
@@ -667,7 +667,41 @@ require('lazy').setup({
       require('mini.surround').setup()
 
       local statusline = require('mini.statusline')
-      statusline.setup({ use_icons = true })
+
+      statusline.setup({
+        use_icons = true,
+        content = {
+          active = function()
+            local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+            -- local git = statusline.section_git({ trunc_width = 40 })
+            local diff = statusline.section_diff({ trunc_width = 75 })
+            local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+            local lsp = statusline.section_lsp({ trunc_width = 75 })
+            local filename = statusline.section_filename({ trunc_width = 140 })
+            local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+            local location = statusline.section_location({ trunc_width = 75 })
+            local search = statusline.section_searchcount({ trunc_width = 75 })
+
+            return statusline.combine_groups({
+              { hl = mode_hl, strings = { mode } },
+              {
+                hl = 'statuslineDevinfo',
+                strings = {
+                  -- git,
+                  diff,
+                  diagnostics,
+                  lsp,
+                },
+              },
+              '%<', -- Mark general truncate point
+              { hl = 'statuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'statuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            })
+          end,
+        },
+      })
 
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
