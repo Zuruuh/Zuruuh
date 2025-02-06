be734e052ce2b3383cdde61c72095d02b81b1578
Zuruuh <zuruh.dev@proton.me>
2025-02-06 09:12:02 +0100

    Ensure nvim is used to edit zellij scrollback

--- nixos/env.nix
+++ nixos/env.nix
@@ -3,7 +3,7 @@
   XDG_CONFIG_HOME = "$HOME/.dotfiles/home/.config";
   XDG_DATA_HOME = "$HOME/.local/share";
   XDG_STATE_HOME = "$HOME/.local/state";
-  EDITOR = "${pkgs.unstable.neovim}/bin/nvim";
+  EDITOR = "/run/current-system/sw/bin/nvim";
   PAGER = "${pkgs.less}/bin/less -R";
   LANG = "en_US.UTF-8";
   LC_ALL = LANG;
