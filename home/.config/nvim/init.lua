e8e4037956f95168773590deb6dd4e93edf6207e
Zuruuh <zuruh.dev@proton.me>
2025-11-06 11:58:15 +0100

    Resolve deprecations

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -683,7 +683,7 @@ require('lazy').setup({
               callback = vim.lsp.buf.clear_references,
             })
 
-            if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
               local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
               vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 buffer = event.buf,
@@ -705,7 +705,7 @@ require('lazy').setup({
                 end,
               })
 
-              if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+              if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
                 map('<leader>th', function()
                   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
                 end, '[T]oggle Inlay [H]ints')
